8bd4b79722308e0b5300c648950b12902eebf1a1
Diego Rojas <diegorojas431@gmail.com>
2021-12-09 17:52:08 -0500

    Arreglando el README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Encanto es Colombia
+
+This project is mede with HTML, CSS and Vanilla JavaScript for learn you about Colombia in Encanto.
--- js/Button.js
+++
@@ -1,7 +0,0 @@
-const Button = ($button, toggle, callback, callback_open) => {
-  $button.addEventListener('click', e => {
-  toggle++;
-  callback(callback_open)
-},
-
-export default Button;
